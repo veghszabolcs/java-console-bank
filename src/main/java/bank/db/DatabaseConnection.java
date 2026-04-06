a7e0a8b111a347fc962e2337745d6c57142476be
Végh Szabolcs <144017614+veghszabolcs@users.noreply.github.com>
2026-04-06 13:02:09 +0200

    added db credentials to .env

--- src/main/java/bank/db/DatabaseConnection.java
+++ src/main/java/bank/db/DatabaseConnection.java
@@ -1,14 +1,18 @@
 package bank.db;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/bank";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    static Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
